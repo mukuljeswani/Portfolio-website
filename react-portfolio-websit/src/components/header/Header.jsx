aaa2b20e0c01eea4741feac37302679b41eee238
mukuljeswani <mukuljeswani@gmail.com>
2022-02-27 13:23:49 -0500

    header is done

--- react-portfolio-websit/src/components/header/Header.jsx
+++ react-portfolio-websit/src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
+import Me from '../../assets/Me.jpeg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mukul Jeswani</h1>
+        <h5 className="text-light">Fullstack Developer </h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={Me} alt="My photo" />
+        </div>
+
+        <a href="#contact" className="scroll__down"> Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
